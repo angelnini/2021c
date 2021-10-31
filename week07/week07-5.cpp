664ee6913cef774f18b57e7f43ae9c669d93522b
angelnini <91250186+angelnini@users.noreply.github.com>
2021-10-31 18:34:44 +0800

    Add files via upload

---
+++ week07/week07-5.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=0; i<3; i++){
+        printf("筿福癹伴程糶猭:%d\n",i);
+    }
+    for(int i=1; i<=3;i++){
+        printf("摸计计眖1计:%d\n",i);
+    }
+}
